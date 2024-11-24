cc84bba23cf748b32cd51a9c76f45ede62912569
apoorvarashi <145325204+apoorvarashi@users.noreply.github.com>
2024-11-24 16:45:47 +0530

    Update anagram.js day 1 abhi

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,20 @@
 /*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  Write a function `isAnagram` which takes 2 parameters (and returns true/false if those are anagrams or not.
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
 function isAnagram(str1, str2) {
-
+// str1 = str1.replace(/\s+/g, '').tolowercase();
+ //str2 = str2.replace(/\s+/g, '').tolowercase();
+ const sortstr1 = str1.split('').sort().join('');
+    const sortstr2 = str2.split('').sort().join('');
+  if(str1.length !== str2.length)
+    return false;
 }
+const sortstr1 = str1.split('').sort().join('');
+const sortstr2 = str2.split('').sort().join('');
+return  sortstr2== sortstr2;
 
-module.exports = isAnagram;
+console.log(isAnagram("listen", "silent")); // true
+console.log(isAnagram("hello", "world"));   // false
